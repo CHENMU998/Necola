410d047130d830101fb915fbe4bd79581ce7aefa
CHENMU998 <2689725079@qq.com>
2026-08-01 23:20:49 +0800

    Update BaseEntity.cpp

--- necola/hook/Raw/BaseEntity/BaseEntity.cpp
+++ necola/hook/Raw/BaseEntity/BaseEntity.cpp
@@ -3,33 +3,34 @@
 
 using namespace Hooks;
 
-
-
 void __fastcall BaseEntity::SetParents::Detour(C_BaseEntity* pThis, void* edx, C_BaseEntity* pParentEntity, int iParentAttachment)
 {
-	
-	if(pThis->IsBaseCombatWeapon()) {
-		C_BaseCombatWeapon* pCombatWeapon = pThis->MyCombatWeaponPointer();
+    if (pParentEntity == nullptr && pThis && pThis->entindex() != -1) {
+        if (G::WeaponPoly.containsEntity(pThis->entindex())) {
+            G::WeaponPoly.removeEntity(pThis->entindex());
+            spdlog::debug("[NecolaPolymorphis]BaseEntity::SetParents-> Removed entity {} from poly map on detach", pThis->entindex());
+        }
+    }
 
-		spdlog::debug("[NecolaPolymorphis]BaseEntity::SetParents-> GetWorldModel:[{}]", pCombatWeapon->GetWorldModel());
-	}
+    if(pThis->IsBaseCombatWeapon()) {
+        C_BaseCombatWeapon* pCombatWeapon = pThis->MyCombatWeaponPointer();
+        if (pCombatWeapon) {
+            spdlog::debug("[NecolaPolymorphis]BaseEntity::SetParents-> GetWorldModel:[{}]", pCombatWeapon->GetWorldModel());
+        }
+    }
 
-	Func.Original<FN>()(pThis, edx, pParentEntity, iParentAttachment);
+    Func.Original()(pThis, edx, pParentEntity, iParentAttachment);
 }
 
 void BaseEntity::Init()
 {
-	//precompute spherical harmonic coefficients for ambient lighting
-	{
-		using namespace SetParents;
+    {
+        using namespace SetParents;
 
-		const FN pfSetParents = reinterpret_cast<FN>(U::Offsets.m_dwSetParents);
-		if( pfSetParents ) {
-			Func.Init(pfSetParents, &Detour);
-		}
-		
-	}
+        const FN pfSetParents = reinterpret_cast<FN>(U::Offsets.m_dwSetParents);
+        if( pfSetParents ) {
+            Func.Init(pfSetParents, &Detour);
+        }
 
-	// FireBullets and TraceAttack GPU command stream intercepts removed - no longer needed
-	// DamageShower now uses m_iHealth RecvProxy GPU command stream intercept instead
+    }
 }
